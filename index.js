ae42ffa8ad4182cafa41913c611552e069e477b2
Stan Chan <stanchan@gmail.com>
2017-04-15 17:03:07 -0700

    Update plugin properties

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = function(kibana) {
 	return new kibana.Plugin({
+		id: 'ob-kb-percent',
+		name: 'ob-kb-percent',
+		require: ['kibana', 'elasticsearch'],
 		uiExports: {
 			visTypes: ['plugins/ob-kb-percent/ob-kb-percent']
 		}
