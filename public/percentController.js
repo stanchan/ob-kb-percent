1a306ca1624e50f2d06786db38e4ec69ec4e7188
Stan Chan <stanchan@gmail.com>
2017-04-17 09:44:41 -0700

    Support inverted precentages

--- public/percentController.js
+++ public/percentController.js
@@ -4,38 +4,38 @@ var numeral = require('numeral');
 
 module.controller('PercentController', function($scope, Private) {
 
-    const tabifyAggResponse = Private(require('ui/agg_response/tabify/tabify'));
+	const tabifyAggResponse = Private(require('ui/agg_response/tabify/tabify'));
 
-    $scope.getValueFromAggs = function (resp, tableGroups, type, params) {
-    	if (type === 'total') {
-    		return resp.hits.total;
-    	}
-    	if (type === 'namedBucket') {
-    		for (var i = 0; i < tableGroups.tables.length; i++) {
-    			const table = tableGroups.tables[i];
-    			for (var j = 0; j < table.rows.length; j++) {
-    				const row = table.rows[j];		
-	        		const bucketName = row[0];
-	    	    	const bucketValue = row[1];
-		        	if (bucketName.toString() === params.namedBucket) {
-	        			return bucketValue;
-		        	}
-    			}
-    		}
-	      return 0.0;
-    	}
-    	if (type === 'nthBucket') {
-    		const table = tableGroups.tables[0];
-    		const bucket = table.rows[params.nthBucket - 1]; // one based.
-    		return bucket[1];
-    	}
-    	return 0;
-    };
+	$scope.getValueFromAggs = function (resp, tableGroups, type, params) {
+		if (type === 'total') {
+			return resp.hits.total;
+		}
+		if (type === 'namedBucket') {
+			for (var i = 0; i < tableGroups.tables.length; i++) {
+				const table = tableGroups.tables[i];
+				for (var j = 0; j < table.rows.length; j++) {
+					const row = table.rows[j];		
+					const bucketName = row[0];
+					const bucketValue = row[1];
+					if (bucketName.toString() === params.namedBucket) {
+						return bucketValue;
+					}
+				}
+			}
+		  return 0.0;
+		}
+		if (type === 'nthBucket') {
+			const table = tableGroups.tables[0];
+			const bucket = table.rows[params.nthBucket - 1]; // one based.
+			return bucket[1];
+		}
+		return 0;
+	};
 
-    $scope.$watch('esResponse', function (resp) {
-      if (resp) {
-        var tabified = tabifyAggResponse($scope.vis, resp);
-        console.log(tabified);
+	$scope.$watch('esResponse', function (resp) {
+	  if (resp) {
+		var tabified = tabifyAggResponse($scope.vis, resp);
+		console.log(tabified);
 
 		var numeratorType = $scope.vis.params.numeratorType;
 		var numeratorParams = $scope.vis.params.numerator;
@@ -46,10 +46,11 @@ module.controller('PercentController', function($scope, Private) {
 		var denominator = $scope.getValueFromAggs(resp, tabified, denominatorType, denominatorParams);
 
 		var ratio = numerator / denominator;
-		if ($scope.vis.params.displayIncrement == true) { ratio = ratio - 1 };
-	        console.log("numerator = ", numerator);
+		if ($scope.vis.params.displayType == 'increment') { ratio = ratio - 1 };
+		if ($scope.vis.params.displayType == 'invert') { ratio = 1 - ratio };
+		console.log("numerator = ", numerator);
 		console.log("denominator = ", denominator);
 		$scope.ratio = numeral(ratio).format($scope.vis.params.format);
-      }
-    });
+	  }
+	});
   });
